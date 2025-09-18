094cdeff9f4c2487c884f539afb06925e95d9a62
enoam <ehabkholif@gmal.com>
2025-09-18 16:17:41 +0200

    n1

--- comm/src/main/java/com/onlineStoreCom/entity/exception/ShippingRateNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.onlineStoreCom.entity.exception;
-
-public class ShippingRateNotFoundException extends Exception {
-
-	public ShippingRateNotFoundException(String message) {
-		super(message);
-	}
-
-}
--- comm/src/main/java/com/onlineStoreCom/entity/order/PaymentMethod.java
+++
@@ -1,5 +0,0 @@
-package com.onlineStoreCom.entity.order;
-
-public enum PaymentMethod {
-	COD, CREDIT_CARD, PAYPAL
-}
